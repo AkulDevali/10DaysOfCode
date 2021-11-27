57bce9a6ae2eabe115e9f58889485b17706436be
Akul G <64837282+AkulDhruv@users.noreply.github.com>
2021-11-27 23:38:01 +0530

    Day 7

--- FibonacciTriangle.c
+++ FibonacciTriangle.c
@@ -3,7 +3,7 @@
 int main(){  
    int a=0,b=1,i,c,n,j; 
     printf("Enter the limit:");    
-    scanf("%d  ",&n);    
+    scanf("%d\n",&n);    
     for(i=1;i<=n;i++)    
     {    
         a=0;    
@@ -12,7 +12,7 @@ int main(){
         for(j=1;j<i;j++)    
         {    
             c=a+b;    
-            printf("%d\t",c);    
+            printf("%d  ",c);    
             a=b;    
             b=c;
         }    
