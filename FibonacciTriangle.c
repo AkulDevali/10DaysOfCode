b0597e3fe60b6ff542a90743907fe02366178027
Akul G <64837282+AkulDhruv@users.noreply.github.com>
2021-11-27 23:36:56 +0530

    Day 7

--- FibonacciTriangle.c
+++ FibonacciTriangle.c
@@ -1,22 +1,20 @@
 #include<stdio.h>    
 #include<stdlib.h>  
 int main(){  
-   int a=0,b=1,i,c,n,j;    
-system("cls");  
+   int a=0,b=1,i,c,n,j; 
     printf("Enter the limit:");    
-    scanf("%d",&n);    
+    scanf("%d  ",&n);    
     for(i=1;i<=n;i++)    
     {    
         a=0;    
         b=1;    
-        printf("%d\t",b);    
+        printf("%d  ",b);    
         for(j=1;j<i;j++)    
         {    
             c=a+b;    
             printf("%d\t",c);    
             a=b;    
-            b=c;    
-
+            b=c;
         }    
         printf("\n");    
     }    
